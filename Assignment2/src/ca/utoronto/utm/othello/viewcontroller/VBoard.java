2be8e7f182058d1a48470d6185fade9cf62d1d0c
Minh Hong Trinh <lilotrinhh@gmail.com>
2019-10-30 17:42:07 -0400

    'abc'

---
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/VBoard.java
@@ -0,0 +1,45 @@
+package ca.utoronto.utm.othello.viewcontroller;
+
+import ca.utoronto.utm.othello.model.Othello;
+import ca.utoronto.utm.util.Observable;
+import ca.utoronto.utm.util.Observer;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class VBoard extends Label implements Observer {
+
+	private Othello othello;
+	private GridPane grid;
+
+	public VBoard(Othello othello, GridPane grid) {
+		this.othello = othello;
+		this.grid = grid;
+	}
+
+	@Override
+	public void update(Observable o) {
+		for (int row = 0; row < Othello.DIMENSION; row++) {
+			for (int col = 0; col < Othello.DIMENSION; col++) {
+				Button button = new Button();
+				for (Node node : this.grid.getChildren()) {
+					if (this.grid.getColumnIndex(node) == col) {
+						if (this.grid.getRowIndex(node) == row) {
+							button = (Button) node;
+							if (this.othello.getToken(row, col) == 'X') {
+								button.setText("X");
+							}
+							if (this.othello.getToken(row, col) == 'O') {
+								button.setText("O");
+							}
+							button.setPrefSize(40, 40);
+							button.setOnAction(new ButtonPressEventHandler(row, col, this.othello));
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
